660b390e9eaddd44bebd49b92d763585871b0124
Ben Mulhern <ben@benmulhern.co.uk>
2019-10-15 21:17:45 +0100

    Add redux, routing etc

---
+++ src/components/Placeholder.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Placeholder = () => (
+  <div>
+    <h1>Shopping List</h1>
+    <p>Here's a shopping list app</p>
+  </div>
+)
+
+export default Placeholder
---
+++ src/reducers/loginReducer.js
@@ -0,0 +1,28 @@
+import Immutable from 'immutable'
+import cloneDeep from "lodash.clonedeep"
+
+const initialState = {
+  loggedIn: false
+}
+
+const loginReducer = (state = initialState, action) => {
+
+  switch (action.type) {        
+
+    case 'LOGIN':
+      return {
+        ...state,
+        loggedIn: true
+      }
+      case 'LOGOUT':
+        return {
+          ...state,
+          loggedIn: false
+        }  
+            
+    default:
+      return state
+  }
+}
+
+export default loginReducer
