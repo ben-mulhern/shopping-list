5ef087566332e8d2082fb17ac7d889b08f078a04
Ben Mulhern <ben@benmulhern.co.uk>
2019-11-07 13:41:19 +0000

    More work on meal detail

--- src/components/MealDetail.js
+++ src/components/MealDetail.js
@@ -10,15 +10,18 @@ import RadioGroup from '@material-ui/core/RadioGroup'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import FormControl from '@material-ui/core/FormControl'
 import FormLabel from '@material-ui/core/FormLabel'
+import Switch from '@material-ui/core/Switch'
+import Slider from '@material-ui/core/Slider'
+import MealIngredient from './MealIngredient'
 
 const useStyles = makeStyles(theme => ({
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: 200
-  },
   formControl: {
-    margin: theme.spacing(3)
+    margin: theme.spacing(3),
+    display: 'block'
+  },
+  width300: {
+    minWidth: 300,
+    maxWidth: 600
   }  
 }))
 
@@ -57,7 +60,7 @@ const emptyMeal = {
   "meal_id": 0,
   "description": "",
   "diet_type": "",
-  "leftovers": "",
+  "leftovers": false,
   "image_url": "",
   "serves": 0,
   "recipe_book": "",
@@ -80,13 +83,26 @@ const ShoppingList = (props) => {
 
   const meal = (called ? data.meal[0] : emptyMeal)
 
+  const marks = [
+    {value: 1, label: "1"},
+    {value: 2, label: "2"},
+    {value: 3, label: "3"},
+    {value: 4, label: "4"},
+    {value: 5, label: "5"},
+    {value: 6, label: "6"},
+    {value: 7, label: "7"},
+    {value: 8, label: "8"}
+  ]
+
   return (
-    <div>
-      <FormControl component="fieldset" className={classes.formControl}>
-        <TextField required label="Description" value={meal.description}
-          className={classes.textField} margin="normal" variant="outlined"
+    <div className={classes.width300}>
+      
+      <FormControl className={classes.formControl}>
+        <TextField required label="Description" value={meal.description} autoFocus
+          fullWidth margin="normal" variant="outlined"
           placeholder="Description" />
       </FormControl>  
+
       <FormControl component="fieldset" className={classes.formControl}>
         <FormLabel component="legend">Diet type</FormLabel>  
         <RadioGroup row name="diet_type" value={meal.diet_type}>
@@ -95,6 +111,49 @@ const ShoppingList = (props) => {
           <FormControlLabel value="VEGAN" control={<Radio />} label="Vegan" />
         </RadioGroup>
       </FormControl>  
+
+      <FormControl component="fieldset" className={classes.formControl}>
+        <FormLabel component="legend">Leftovers</FormLabel>        
+        <Switch
+          checked={meal.leftovers}
+          value={meal.leftovers}
+          color="primary"
+        />
+      </FormControl>     
+
+      <FormControl component="fieldset" className={classes.formControl}>
+        <TextField label="Image URL" value={meal.image_url}
+          fullWidth margin="normal" variant="outlined"
+          placeholder="Image URL" />
+      </FormControl>  
+
+      <FormControl component="fieldset" className={classes.formControl}>
+        <FormLabel component="legend">Serves</FormLabel>   
+        <Slider defaultValue={4}
+                step={1}
+                marks={marks}
+                min={1}
+                max={8}
+                value={meal.serves}
+                valueLabelDisplay="on"
+        />
+      </FormControl>   
+
+      <FormControl component="fieldset" className={classes.formControl}>
+        <TextField label="Recipe book" value={meal.recipe_book}
+          fullWidth margin="normal" variant="outlined"
+          placeholder="Recipe book" />
+      </FormControl>  
+
+      <FormControl component="fieldset" className={classes.formControl}>
+        <TextField label="Tags" value={meal.meal_tags.map(t => t.tag).join(" ")}
+          fullWidth margin="normal" variant="outlined"
+          placeholder="Tags" />
+      </FormControl>  
+
+      <h2>Ingredients</h2>
+      {meal.meal_ingredients.map(mi => <MealIngredient mealIngredient={mi} />)}
+
     </div>
   )
 }
