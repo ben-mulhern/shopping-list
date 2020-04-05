f45557a4a97319bcfbc0e5c572738f9418e68cd2
Ben Mulhern <ben@benmulhern.co.uk>
2020-04-05 20:58:05 +0100

    This is bullshit

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -6,8 +6,6 @@ import { getMainDefinition } from 'apollo-utilities'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { setContext } from 'apollo-link-context'
 
-const apiKey = sessionStorage.getItem('API_KEY')
-
 const httpLink = createHttpLink({  
   uri: process.env.REACT_APP_API_URL
 })
@@ -15,16 +13,19 @@ const httpLink = createHttpLink({
 const wsLink = new WebSocketLink({
   uri: process.env.REACT_APP_API_WS_URL, 
   options: {
+    lazy: true,
     reconnect: true,
     connectionParams: {
       headers: {
-        "x-hasura-admin-secret": apiKey || ''
+        "x-hasura-admin-secret": sessionStorage.getItem('API_KEY') || ''
       }
     }
-  }
+  } 
 })
 
 const authLink = setContext((_, { headers }) => {
+  console.log("Authlink called")
+  const apiKey = sessionStorage.getItem('API_KEY')
   return {
     headers: {
       ...headers,
