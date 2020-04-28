78be45a82d988b6372036cbbb5b47ade331846fe
Ben Mulhern <ben@benmulhern.co.uk>
2020-04-28 21:30:08 +0100

    Test deployment to see what's up with Heroku

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import React from 'react'
-import MainView from './components/MainView'
-import {Provider} from 'react-redux'
-import configureStore from './state/configureStore'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import {BrowserRouter} from 'react-router-dom'
-import {ApolloProvider} from '@apollo/react-hooks'
-import apiClient from './api/apiClient'
 
 const App = (props) => (
-  <Provider store={configureStore()}> 
-    <ApolloProvider client={apiClient}>
-      <React.Fragment>
-        <CssBaseline />   
-        <BrowserRouter>
-          <MainView />
-        </BrowserRouter> 
-      </React.Fragment>  
-    </ApolloProvider>
-  </Provider>  
+  <p>Test</p>
 )
 
 export default App
